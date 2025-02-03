804bdc9a84d536d812010a2bef98bac84620022b
lyra <mk.lyra.j@gmail.com>
2025-02-04 00:31:04 +0900

    feat: Mock_data받아와서 리스트보드에 그려주기완료

--- src/pages/Dex.jsx
+++ src/pages/Dex.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React from "react";
+import MOCK_DATA from "../data/MOCK_DATA";
 
 const Dex = () => {
   return (
-    <div>Dex</div>
-  )
-}
+    <div>
+      {/* 대쉬보드 (6장) */}
+      <div>
+        <h1>나만의 포켓몬</h1>
+        <ul></ul>
+      </div>
 
-export default Dex
+      {/* 포켓몬 리스트 */}
+      <div>
+        <ul>
+          {MOCK_DATA.map((data) => {
+            return (
+              <li key={data.id}>
+                <img src={data.img_url} alt={data.korean_name} />
+                <h3>{data.korean_name}</h3>
+                <p>no.{data.id}</p>
+                <button onClick={() => handleAddPokemon(data)}>+ add</button>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Dex;
