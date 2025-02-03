652a18dcfddcbcb209ff19aafa720cd830dbb24c
lyra <mk.lyra.j@gmail.com>
2025-02-04 00:33:40 +0900

    feat: 포켓몬덱에 추가 handle 구현

--- src/pages/Dex.jsx
+++ src/pages/Dex.jsx
@@ -2,6 +2,28 @@ import React from "react";
 import MOCK_DATA from "../data/MOCK_DATA";
 
 const Dex = () => {
+  const [selectedPokemon, setSelectedPokemon] = useState([]);
+
+  // 포켓몬 선택하여 덱에 추가
+  const handleAddPokemon = (pokemon) => {
+    const isAlreadySelectedOne = selectedPokemon.find((item) => {
+      return item.id === pokemon.id;
+    });
+
+    if (isAlreadySelectedOne) {
+      alert(`이미 보유한 포켓몬입니다.`);
+      return;
+    }
+
+    if (selectedPokemon.length >= 6) {
+      alert(`포켓몬은 최대 여섯마리까지 선택 할 수 있어요.`);
+      return;
+    }
+
+    setSelectedPokemon([...selectedPokemon, pokemon]);
+  };
+
+  
   return (
     <div>
       {/* 대쉬보드 (6장) */}
