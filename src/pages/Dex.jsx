d8e5546febee23fb0e1c6011d670c1277a439177
lyra <mk.lyra.j@gmail.com>
2025-02-04 13:38:50 +0900

    style: ui기본 정렬 셋팅

--- src/pages/Dex.jsx
+++ src/pages/Dex.jsx
@@ -1,10 +1,11 @@
 import React, { useState } from "react";
+import styled from "styled-components";
 import MOCK_DATA from "../data/MOCK_DATA";
 
 const Dex = () => {
   const [selectedPokemon, setSelectedPokemon] = useState([]);
 
-  // 포켓몬 대쉬보드에 추가
+  // ✅ 포켓몬 대쉬보드에 추가
   const handleAddPokemon = (pokemon) => {
     const isSelected = selectedPokemon.find((item) => {
       return item.id === pokemon.id;
@@ -23,7 +24,7 @@ const Dex = () => {
     setSelectedPokemon([...selectedPokemon, pokemon]);
   };
 
-  // 포켓몬 대쉬보드에서 삭제
+  // ✅ 포켓몬 대쉬보드에서 삭제
   const handleRemovePokemon = (id) => {
     setSelectedPokemon((prevList) =>
       prevList.filter((pokemon) => pokemon.id !== id)
@@ -31,27 +32,27 @@ const Dex = () => {
   };
 
   return (
-    <div>
+    <DexWrapper>
       {/* 대쉬보드 (6장) */}
-      <div>
-        <h1>나만의 포켓몬</h1>
-        <ul>
+      <DashBoardWrapper>
+        <MyPokemonTitle>나만의 포켓몬</MyPokemonTitle>
+        <MyPokemonCardBox>
           {selectedPokemon.map((item, idx) => {
             return (
-              <li key={idx}>
+              <MyPokemonCard key={idx}>
                 <img src={item.img_url} alt={item.korean_name} />
                 <h3>{item.korean_name}</h3>
                 <p>no.{item.id}</p>
                 <button onClick={() => handleRemovePokemon(item.id)}>
                   - del
                 </button>
-              </li>
+              </MyPokemonCard>
             );
           })}
 
           {new Array(6 - selectedPokemon.length).fill(null).map((_, idx) => {
             return (
-              <li key={idx}>
+              <MonsterBall key={idx}>
                 <img
                   style={{
                     width: "50px",
@@ -59,29 +60,104 @@ const Dex = () => {
                   src="src/assets/pokeball.png"
                   alt="empty slot"
                 />
-              </li>
+              </MonsterBall>
             );
           })}
-        </ul>
-      </div>
+        </MyPokemonCardBox>
+      </DashBoardWrapper>
 
       {/* 포켓몬 리스트 */}
-      <div>
-        <ul>
-          {MOCK_DATA.map((data) => {
-            return (
-              <li key={data.id}>
-                <img src={data.img_url} alt={data.korean_name} />
-                <h3>{data.korean_name}</h3>
-                <p>no.{data.id}</p>
-                <button onClick={() => handleAddPokemon(data)}>+ add</button>
-              </li>
-            );
-          })}
-        </ul>
-      </div>
-    </div>
+      <PokemonListWrapper>
+        {MOCK_DATA.map((data) => {
+          return (
+            <PokemonCard key={data.id}>
+              <img src={data.img_url} alt={data.korean_name} />
+              <h3>{data.korean_name}</h3>
+              <p>no.{data.id}</p>
+              <button onClick={() => handleAddPokemon(data)}>+ add</button>
+            </PokemonCard>
+          );
+        })}
+      </PokemonListWrapper>
+    </DexWrapper>
   );
 };
 
 export default Dex;
+
+const DexWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 20px;
+`;
+
+const PokemonListWrapper = styled.ul`
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(150px, 1fr));
+  gap: 20px;
+  background-color: rgb(240, 240, 240);
+  padding: 20px;
+  border: 1px solid rgb(221, 221, 221);
+  border-radius: 10px;
+`;
+
+const PokemonCard = styled.li`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background-color: white;
+  box-shadow: 0px 3px 8px rgba(0, 0, 0, 0.3);
+  border-radius: 15px;
+  width: 170px;
+  height: 250px;
+`;
+
+const DashBoardWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: #f8f8f8;
+  padding: 20px;
+  border-radius: 10px;
+  margin-bottom: 30px;
+`;
+
+const MyPokemonTitle = styled.h1`
+  font-size: 30px;
+  font-weight: 600;
+  color: #ff0000da;
+  margin: 40px;
+`;
+
+const MyPokemonCardBox = styled.ul`
+  display: grid;
+  grid-template-columns: repeat(6, 1fr);
+  gap: 20px;
+  width: 100%;
+  justify-items: center;
+  margin-bottom: 10px;
+`;
+
+const MyPokemonCard = styled.li`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background-color: white;
+  box-shadow: 0px 3px 8px rgba(0, 0, 0, 0.3);
+  border-radius: 15px;
+  width: 150px;
+  height: 200px;
+`;
+
+const MonsterBall = styled.li`
+  width: 100px;
+  height: 100px;
+  background-color: rgb(255, 255, 255);
+  border: 2px dashed rgb(204, 204, 204);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 10px;
+`;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,20 +1,52 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
 
 const Home = () => {
   const navigate = useNavigate();
+
   return (
-    <div>
-      pokemon log
-      <button
+    <Wrapper>
+      <LogoImage src="src/assets/pokemon-logo.png" alt="PekemonDex" />
+      <BigButton
         onClick={() => {
           navigate("/dex");
         }}
       >
         포켓몬 도감 시작하기
-      </button>
-    </div>
+      </BigButton>
+    </Wrapper>
   );
 };
 
 export default Home;
+
+const Wrapper = styled.div`
+  width: 100vw;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  min-height: 100dvh;
+`;
+
+const LogoImage = styled.img`
+  height: 260px;
+`;
+
+const BigButton = styled.button`
+  width: 200px;
+  height: 50px;
+  background-color: red;
+  color: white;
+  margin-top: 50px;
+  border: solid 1px;
+  border-radius: 8px;
+  font-size: 20px;
+
+  &:hover {
+    background-color: #d12c2c;
+    transition: 0.5s;
+    cursor: pointer;
+  }
+`;
